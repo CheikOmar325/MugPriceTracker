f1336e6165cbcac4851fe6a4be960db12b3c4f97
Cheik Omar Saka <78073693+CheikOmar325@users.noreply.github.com>
2022-08-23 10:31:44 +0100

    The email feature has been implemented and works

--- Pricetracker.py
+++ Pricetracker.py
@@ -3,10 +3,12 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import ssl
 from email.message import EmailMessage
 
 URL = 'https://shopatlus.com/collections/persona-5-royal/products/magic-mug'
-headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36'}
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/104.0.0.0 Safari/537.36'}
 
 page = requests.get(URL, headers=headers)
 
@@ -14,20 +16,38 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 Title = soup.find(itemprop='name', class_="product-single__title").get_text()
 price = soup.find(id="ProductPrice-product-template").get_text()
-float_price= float(price[1:])
+float_price = float(price[1:])
 
-print(Title,"", float_price)
+print(Title, "", float_price)
 
-#I will be setting up an email notification for when the mug's price changes
-#In this version of the code the email sending feature has yet to be fully implemented
+# I will be setting up an email notification for when the mug's price changes
+# In this version of the code the email sending feature has yet to be fully implemented
 msg = EmailMessage()
+
+
 def send_mail():
-    msg ['Subject']= ("The item ", Title, 'is on sale for ', float_price)
-    msg['From'] = 'The Phantom Thieves'
-    msg['To'] = 'sakacheik@gmail.com'
-    msg.set_content('test email from Joker')
-    server= smtplib.SMTP_SSL("smtp@gmail.com", 465)
+    msg = EmailMessage()
+    # qtrtxazfkxljmwct
+    email_sender = 'quandeliciousdinglenut@gmail.com'
+    email_password = 'qtrtxazfkxljmwct'
+    email_receiver = 'kinas62519@ukgent.com'
+    subject = ('The item is on sale! Lets get this treasure!! ')
+    body = "Its on sale!"
+    em = EmailMessage()
+    em['From'] = email_sender
+    em['To'] = email_receiver
+    em['subject'] = subject
+    em.set_content(body)
+    context = ssl.create_default_context()
 
-if (float_price == 16.99):
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
+
+
+if float_price < 16.99:
     print('Hey Joker! Your Mug is on sale! Lookin cool!')
-
+    send_mail()
+elif float_price > 16.99:
+    print('The Price got raised')
+    send_mail()
--- emailtest.py
+++ emailtest.py
@@ -1,13 +1,23 @@
 #I use this to test the email sending process before implementing in my code
 import smtplib
+import ssl
 from email.message import EmailMessage
-msg = EmailMessage()
 
-msg ['Subject']= ('The item is on sale! Lets get this treasure!! ')
-msg['From'] = 'The Phantom Thieves'
-msg['To'] = 'sakacheik@gmail.com'
-msg.set_content('test email from Joker')
-server = smtplib.SMTP_SSL("smtp@gmail.com", 465)
-server.login('quandeliciousdinglenut@gmail.com','mdp12345')
-server.send_message(msg)
-server.quit()
+msg = EmailMessage()
+#qtrtxazfkxljmwct
+email_sender = 'quandeliciousdinglenut@gmail.com'
+email_password = 'qtrtxazfkxljmwct'
+email_receiver = 'kinas62519@ukgent.com'
+subject = ('The item is on sale! Lets get this treasure!! ')
+body = "Its on sale!"
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
+
